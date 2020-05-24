33a3b3c5b7ae07b8dcb3f0070e2daab843f74aa3
erisleci <erisleci19@gmail.com>
2020-05-24 16:31:38 +0200

    Last commit poget

--- scripts/script.js
+++ scripts/script.js
@@ -51,18 +51,131 @@ showLoginButton.addEventListener("click",shfaqeLoginin);
 showRegistrationButton.addEventListener("click",shfaqeRegjistrimin);
 
 
+let slideIndex = 0;
+let slides = document.getElementsByClassName("Slides");
+if(slides.length != 0) {
+    function showSlides() {
+      let i;
+      
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 4000); // Change image every 2 seconds
+    }
+    showSlides();
+}
 
-var slideIndex = 0;
-showSlides();
+    
+ 
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("Slides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
-} 
+
+/* Validation */
+
+   /* Login Validation */
+
+   
+   let loginEmail = document.getElementById("email");
+   let loginPassword = document.getElementById("password");
+   let butoniKyqjes = document.getElementById("loginButton");
+   let errorFieldLogin = document.querySelector(".errorsDivLogin");
+   butoniKyqjes.addEventListener('click', testo);
+
+   function testo() {
+        
+        if(loginEmail.value == "") {
+            errorFieldLogin.innerHTML="Email nuk mund te jete i zbrazet"
+            return;
+        }
+        if(loginPassword.value == "") {
+            errorFieldLogin.innerHTML="Password nuk mund te jete i zbrazet";
+            return;
+        }
+
+        errorFieldLogin.innerHTML="SUCCESS!"
+   }
+
+
+   /* Registration validation */
+
+   let emri = document.getElementById("firstName");
+   let mbiemri = document.getElementById("lastName");
+   let imellaRegjistrimit = document.getElementById("registerEmail");
+   let fjalekalimi = document.getElementById("registerPassword");
+   let konfirmoFjalekalimin = document.getElementById("confirmPassword");
+   let butoniRegjistrimit = document.getElementById("registerButton");
+   let errorFieldRegister = document.querySelector(".errorsDivRegister");
+  // butoniKyqjes.addEventListener("click", testoRegjistrimin);
+
+   let emriValidation = /[0-9.,!@#$%^&*()_-]+/;
+   
+   
+   
+   butoniRegjistrimit.addEventListener("click", testoRegjistrimin);
+
+   function testoRegjistrimin() {
+        
+    //EMRI
+        if(emri.value.length < 2) {
+            errorFieldRegister.innerHTML="GABIM EMRI: Emri duhet ti permbaje se paku 3 shkronja";
+            return;
+        }
+   
+        if(emriValidation.test(emri.value)) {
+            errorFieldRegister.innerHTML="GABIM EMRI: Emri nuk mund te permbaje numra apo simbole";
+            return;
+        } 
+        
+    //MBIEMRI
+        if(mbiemri.value.length < 2) {
+            errorFieldRegister.innerHTML="GABIM MBIEMRI: Emri duhet ti permbaje se paku 3 shkronja";
+            return;
+        }
+   
+        if(emriValidation.test(mbiemri.value)) {
+            errorFieldRegister.innerHTML="GABIM MBIEMRI: Emri nuk mund te permbaje numra apo simbole";
+            return;
+        }
+     
+    //EMAIL ADDRESS
+        if(imellaRegjistrimit.value.length= 0) {
+            errorFieldRegister.innerHTML="Email nuk mund te jete i zbrazet";
+            return;
+        }
+
+        let emailRegex = /[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/
+        if(!emailRegex.test(imellaRegjistrimit.value)) {
+            errorFieldRegister.innerHTML="GABIM EMAIL: Nuk eshte ne formatin e duhur";
+            return;
+        }
+
+     //Password
+        if(fjalekalimi.value == "") {
+            errorFieldRegister.innerHTML="GABIM PASSWORD: Nuk mund te jete i zbrazet";
+            return;
+        }
+        if(fjalekalimi.value.length < 6) {
+            errorFieldRegister.innerHTML="GABIM PASSWORD: Duhet te jete se paku 6 karaktere";
+            return;
+        }
+        let passRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+
+        if(!fjalekalimi.value.match(passRegex)) {
+            errorFieldRegister.innerHTML="GABIM PASSWORD: Password duhet te permbaje se paku nje shkronje te madhe dhe nje simbol.";
+            return;
+        }
+
+        if(!(fjalekalimi.value == konfirmoFjalekalimin.value)) {
+            errorFieldRegister.innerHTML="Passwordi nuk eshte i njejte"
+            return;
+        }
+
+        errorFieldRegister.innerHTML="SUCCESS!";
+    }
+   
+    
+
+   
+   
